5cf9dc89e8810bb0642985311554a5eb4b4391af
huulocnguyen96 <68378083+huulocnguyen96@users.noreply.github.com>
2020-08-02 14:42:15 +0100

    Update RandomDot.py

--- RandomDot.py
+++ RandomDot.py
@@ -1,6 +1,26 @@
 # importing necessary libraries
 from psychopy import visual, core
 import random
+import spidev
+
+# Open SPI bus
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz=1000000
+
+# Function to read SPI data from MCP3008 chip
+# Channel must be an integer 0-7
+def read_channel(channel):
+  adc = spi.xfer2([1,(8+channel)<<4,0])
+  data = ((adc[1]&3) << 8) + adc[2]
+  return data
+
+# Function to convert data to voltage level,
+# rounded to specified number of decimal places.
+def ConvertVolts(data,places):
+  volts = (data * 3.3) / float(1023)
+  volts = round(volts,places)
+  return volts
 
 #create a window
 mywin = visual.Window([800,600], monitor="testMonitor", units="deg")
@@ -10,24 +30,34 @@ rangeX = (-7, 7)
 rangeY = (-7, 7)
 qty = 1
 
-# setting up function to print random coordinates
-cords = []
-excluded = set()
-i = 0
-while i<qty:
-    x = random.randrange(*rangeX)
-    y = random.randrange(*rangeY)
-    if (x,y) in excluded: continue
-    cords.append((x,y))
-    i += 1
-print(cords)
+frame = 7.5
+seconds_stim = 1
+frame_rate = int(mywin.getActualFrameRate())
 
-# create some stimuli
-fixation = visual.GratingStim(win=mywin, mask="circle", size=2, pos= cords, sf=0, rgb=-1)
+for i in range (30):
 
-# draw the stimuli and update the window
-fixation.draw()
-mywin.update()
+    # setting up function to print random coordinates
+    cords = []
+    excluded = set()
+    i = 0
+    while i<qty:
+        x = random.randrange(*rangeX)
+        y = random.randrange(*rangeY)
+        if (x,y) in excluded: continue
+        cords.append((x,y))
+        i += 1
+    print(cords, file=open("cords.txt", "a"))
 
-# pause (in s)
-core.wait(5)
+    for i in range (4):
+        # create some stimuli
+        fixation = visual.GratingStim(win=mywin, mask="circle", size=2, pos= cords, sf=0, rgb=-1)
+        inverse_fixation = visual.GratingStim(win=mywin, mask="circle", size=2, pos= cords, sf=0, rgb=0)
+
+        for frameN in range(frame_rate * seconds_stim):
+            if (frameN % (frame * 2)) >= frame:
+                fixation.draw()
+                #print(read_channel(channel), file=open("cords.txt", "a"))
+            else:
+                inverse_fixation.draw()
+                #print(read_channel(channel), file=open("cords.txt", "a"))
+            mywin.flip()
