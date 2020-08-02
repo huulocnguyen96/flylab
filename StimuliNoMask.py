bda5953002a9324d91686dfab81c0e2e677a6c53
huulocnguyen96 <68378083+huulocnguyen96@users.noreply.github.com>
2020-08-02 14:41:47 +0100

    Update StimuliNoMask.py

--- StimuliNoMask.py
+++ StimuliNoMask.py
@@ -1,11 +1,32 @@
 # importing necessary libraries
 from psychopy import visual, core
+import spidev
+
+# Open SPI bus
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz=1000000
+
+# Function to read SPI data from MCP3008 chip
+# Channel must be an integer 0-7
+def read_channel(channel):
+  adc = spi.xfer2([1,(8+channel)<<4,0])
+  data = ((adc[1]&3) << 8) + adc[2]
+  return data
+
+# Function to convert data to voltage level,
+# rounded to specified number of decimal places.
+def ConvertVolts(data,places):
+  volts = (data * 3.3) / float(1023)
+  volts = round(volts,places)
+  return volts
 
 # create a window
 mywin = visual.Window(monitor="testMonitor", units="deg", rgb=0)
 
 # create some stimuli
 grating = visual.GratingStim(win=mywin, mask="circle", size=4, pos=[0,0], sf=1, contrast=0.6, phase=(0.0, 0.0))
+inverse_grating = visual.GratingStim(win=mywin, mask="circle", size=4, pos=[0,0], sf=1, contrast=-0.6, phase=(0.0, 0.0))
 fixation = visual.GratingStim(win=mywin, pos=[0,0], sf=0, rgb=0)
 
 # calculate framerate of monitor setup
@@ -22,7 +43,7 @@ print(frame_rate)
 # 2.5 frames = 12 hz
 # 2 frames = 15 hz
 # 1 frame = 30 hz
-frame = 30
+frame = 7.5
 
 # duration in seconds for stimulus & buffer
 seconds_stim = 10
@@ -36,7 +57,12 @@ for frameN in range(frame_rate*seconds_buf):
 for frameN in range(frame_rate*seconds_stim):
     if (frameN % (frame*2)) >= frame:
         grating.draw()
+        print(read_channel(channel), file=open("output.txt", "a"))
+    else:
+        inverse_grating.draw()
+        print(read_channel(channel), file=open("output.txt", "a"))
     mywin.flip()
+
 
 # allow stimulus to buffer
 for frameN in range(frame_rate*seconds_buf):
